6b7636b6d9978ebf86dd075d2e27675c9b25b5ad
JacobHong <hungkaojay@gmail.com>
2025-06-16 12:58:39 +0800

    修正上傳作品送出連點兩次重複上傳

---
+++ Calligraphy/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calligraphy.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        //測試抓IP
+        [HttpGet("ip")]
+        public IActionResult GetIP()
+        {
+            var remoteIp = HttpContext.Connection.RemoteIpAddress?.ToString();
+            var forwardedFor = HttpContext.Request.Headers["X-Forwarded-For"].FirstOrDefault();
+
+            return Ok(new
+            {
+                RemoteIp = remoteIp,
+                ForwardedFor = forwardedFor
+            });
+        }
+
+    }
+}
